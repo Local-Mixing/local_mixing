86550ddc7d4cf2f6437fdc56447be4bae8198ed1
hon <hon@bu.edu>
2025-10-22 14:28:44 -0400

    try red blue heatmaps:

--- heatmap/heatmap.py
+++ heatmap/heatmap.py
@@ -26,12 +26,9 @@ def plot_heatmap(data, save_path, xlabel="X-axis", ylabel="Y-axis", vmin=-3, vma
     for xi, yi, z in zip(x, y, z_values):
         heatmap[y_indices[yi], x_indices[xi]] = z
 
-    # Purple = -3, green = 0, red = +3
-    cmap = LinearSegmentedColormap.from_list("purple_green_red", ["purple", "green", "red"])
-
     plt.imshow(
         heatmap,
-        cmap=cmap,
+        cmap='RdBu_r',  # red = high, blue = low
         aspect='auto',
         origin='lower',
         extent=[x_unique[0], x_unique[-1], y_unique[0], y_unique[-1]],
