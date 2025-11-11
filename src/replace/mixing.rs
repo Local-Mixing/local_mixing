bca1950966fb247dfc6376d9d0e6077240ebcbf1
hon <hon@bu.edu>
2025-11-11 00:48:15 -0500

    medium wings

--- src/replace/mixing.rs
+++ src/replace/mixing.rs
@@ -322,11 +322,11 @@ pub fn abutterfly_big(
     let mut rng = rand::rng();
     let mut pre_blocks: Vec<CircuitSeq> = Vec::with_capacity(c.gates.len());
 
-    let (first_r, first_r_inv) = random_id(n as u8, rng.random_range(10..=15));
+    let (first_r, first_r_inv) = random_id(n as u8, rng.random_range(40..=75));
     let mut prev_r_inv = first_r_inv.clone();
 
     for &g in &c.gates {
-        let (r, r_inv) = random_id(n as u8, rng.random_range(10..=15));
+        let (r, r_inv) = random_id(n as u8, rng.random_range(40..=75));
         let block = prev_r_inv.clone().concat(&CircuitSeq { gates: vec![g] }).concat(&r);
         //shoot_random_gate(&mut block, 1_000);
         pre_blocks.push(block);
