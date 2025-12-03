71cab985ffc16fa0a93800b8858cac587c4bca7c
hon <hon@bu.edu>
2025-12-03 14:39:35 -0500

    see how often large convex are chosen

--- src/replace/replace.rs
+++ src/replace/replace.rs
@@ -648,6 +648,7 @@ pub fn compress_big(c: &CircuitSeq, trials: usize, num_wires: usize, conn: &mut 
         for set_size in (3..=13).rev() {
             let random_max_wires = rng.random_range(3..=7);
             let (gates, _) = find_convex_subcircuit(set_size, random_max_wires, num_wires, &circuit, &mut rng);
+            println!("{} found", gates.len());
             if !gates.is_empty() {
                 subcircuit_gates = gates;
                 break;
