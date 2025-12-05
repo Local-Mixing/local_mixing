7d1a1d6c559263d108025d5c7b006ec290d4c045
Nicholas Ho <hon@bu.edu>
2025-12-05 16:17:40 -0500

    start from max

--- src/replace/replace.rs
+++ src/replace/replace.rs
@@ -497,7 +497,7 @@ pub fn expand_lmdb(
             (canon_perm.perm.repr_blob(), canon_perm.shuffle.repr_blob())
         };
         let prefix = canon_perm_blob.as_slice();
-        for smaller_m in (sub_m..min).rev() {
+        for smaller_m in (sub_m..=max).rev() {
             let db_name = format!("n{}m{}", n, smaller_m);
             if (n == 7 && smaller_m == 4) || (n == 6 && smaller_m == 5) {
                 let table = format!("n{}m{}", n, smaller_m);
