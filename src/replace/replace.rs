1711e78f09ecae2218f1f843ee5f1016d8f9e3b2
Nicholas Ho <hon@bu.edu>
2025-12-05 16:32:20 -0500

    fix expand

--- src/replace/replace.rs
+++ src/replace/replace.rs
@@ -1145,7 +1145,7 @@ pub fn expand_big(c: &CircuitSeq, trials: usize, num_wires: usize, conn: &mut Co
 
         let max = 7;
         let new_wires = rng.random_range(num_wires..=max);
-        let subcircuit_temp = expand_lmdb(&subcircuit, 5, conn, &bit_shuf, new_wires, &env, num_wires);
+        let subcircuit_temp = expand_lmdb(&subcircuit, 3, conn, &bit_shuf, new_wires, &env, num_wires);
 
         if subcircuit.permutation(num_wires) != subcircuit_temp.permutation(num_wires) {
             panic!("Compress changed something");
