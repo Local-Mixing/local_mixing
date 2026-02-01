e10800245024800ce84bfaff0f0645002169cdce
hon <hon@bu.edu>
2026-02-01 13:46:35 -0500

    try 128 wire towers

--- src/replace/replace.rs
+++ src/replace/replace.rs
@@ -430,7 +430,7 @@ pub fn get_random_wide_identity(
     let mut nwires = uw.len();
     let mut rng = rand::rng();
     let mut len = 0;
-    while nwires < 16 || len < 160 {
+    while nwires < 16 || len < 500 {
         shoot_random_gate(&mut id, 100_000);
         let gp = GatePair::from_int(rng.random_range(0..34));
         let mut i = match get_random_identity(6, gp, env, dbs) {
@@ -460,6 +460,7 @@ pub fn get_random_wide_identity(
             let mut min_keys: Vec<u8> = wires.keys().cloned().collect();
             min_keys.sort_by_key(|k| wires.get(k).map(|v| v.len()).unwrap_or(0));
             let min = min_vals[0];
+            let min = id.gates.len()/2;
             let mut used_wires = vec![id.gates[min][0], id.gates[min][1], id.gates[min][2]];
             let mut unused_wires: Vec<u8> = (0..n as u8)
                 .filter(|w| !used_wires.contains(w) && !uw.contains(w))
@@ -3866,7 +3867,7 @@ mod tests {
         let dbs = open_all_dbs(&env);
         let mut curr_mean = 0.0;
         loop {
-            let id = get_random_wide_identity_via_pairs(16, &env, &dbs, &mut thread_conn, &bit_shuf_list);
+            let id = get_random_wide_identity(128, &env, &dbs, &mut thread_conn, &bit_shuf_list);
 
             assert!(
                 id.probably_equal(&CircuitSeq { gates: Vec::new() }, 16, 100_000).is_ok(),
