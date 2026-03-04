2cf451efeb6ec5ff5ea6fd0189d8f267ee0d3c08
honbugit <honicholas10@gmail.com>
2026-03-03 22:16:33 -0500

    Test: Test on shuffles with no bit shuffles

--- src/replace/transpositions.rs
+++ src/replace/transpositions.rs
@@ -18,6 +18,7 @@ impl Transpositions {
         let n = (n - 1) as u8;
         for i in (1..=n).rev() {
             let negation_type = rng.random_range(0..=3);
+            let negation_type = 0;
             let j = rng.random_range(0..=i);
             if i == j {
                 continue;
