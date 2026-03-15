c650cbc9ee23852a8e1b998a0ce3e0ab7eb18d3a
honbugit <honicholas10@gmail.com>
2026-03-15 17:40:08 -0400

    Test identity structure

--- src/replace/transpositions.rs
+++ src/replace/transpositions.rs
@@ -1139,6 +1139,7 @@ pub fn insert_ri_identities(c: &mut CircuitSeq, env: &Environment, dbs: &HashMap
         let combined = (t1, new_perm);
         let mut san = combined.0.restricted_to_circuit(32, env, dbs, &vec![13,14,15,29,30,31]);
         san.rewire(&combined.1, 32);
+        println!("{:?}", combined.1);
         if san.probably_equal(&s, 32, 1000).is_err() {
             panic!("Seam fail");
         }
