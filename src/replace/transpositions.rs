6aca6e5d448ff8a7b1a8df2645145394864e6311
honbugit <honicholas10@gmail.com>
2026-03-15 01:32:08 -0400

    Test identity structure

--- src/replace/transpositions.rs
+++ src/replace/transpositions.rs
@@ -1027,18 +1027,14 @@ pub fn insert_ri_identities(c: &mut CircuitSeq, env: &Environment, dbs: &HashMap
     // Create and rewire all the RI identities
     for i in 1..len {
         let (first, middle, second, _, _) = create_ri_identities_32();
-        let f = first.to_circuit(32, &env, &dbs);
-        let mut m = middle.to_circuit(32, &env, &dbs);
+        let mut f = first.clone().to_circuit(32, &env, &dbs);
+        let mut m = middle.clone().to_circuit(32, &env, &dbs);
         m.gates.reverse();
-        let s = second.to_circuit(32, &env, &dbs);
-
-        let mut c = CircuitSeq {gates: Vec::new() };
-        c.gates.extend_from_slice(&f.gates);
-        c.gates.extend_from_slice(&m.gates);
-        c.gates.extend_from_slice(&s.gates);
-        let id = CircuitSeq {gates: Vec::new() };
-        if c.probably_equal(&id, 32, 1000).is_err() {
-            panic!("Not an id");
+        let mut s = second.clone().to_circuit(32, &env, &dbs);
+        f = f.concat(&m).concat(&s);
+        let id = CircuitSeq { gates: Vec::new() };
+        if id.probably_equal(&f, 32, 1000).is_err() {
+            panic!("Not an identity")
         }
         let mut wire_shuffle1 = Permutation{ data: (0..32).collect() };
         for val in used_wires {
