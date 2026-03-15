ec0072f99ac3335b2835426435a9733f816ade32
honbugit <honicholas10@gmail.com>
2026-03-15 02:02:04 -0400

    Test identity structure

--- src/replace/transpositions.rs
+++ src/replace/transpositions.rs
@@ -1077,7 +1077,7 @@ pub fn insert_ri_identities(c: &mut CircuitSeq, env: &Environment, dbs: &HashMap
         let mut sanity = combined.to_circuit(32, &env, &dbs);
         sanity.rewire(&new_perm, 32);
         for j in 0..3 {
-            if sanity.gates.iter().any(|g| g.contains(&c.gates[i][j])) {
+            if sanity.gates.iter().any(|g| g.contains(&c.gates[i+1][j])) {
                 panic!("Not a snug fit");
             }
         }
