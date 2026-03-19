770bac3d4eb3f04918c36b3d843f0f72fcda96a9
honbugit <honicholas10@gmail.com>
2026-03-19 13:42:00 -0400

    Fix: Middle updated with negation mask

--- src/replace/identities.rs
+++ src/replace/identities.rs
@@ -1015,9 +1015,9 @@ mod test {
         let s = second.to_circuit(32, &env, &dbs);
 
         let mut c = CircuitSeq {gates: Vec::new() };
+        c.gates.extend_from_slice(&s.gates);
         c.gates.extend_from_slice(&f.gates);
         c.gates.extend_from_slice(&m.gates);
-        c.gates.extend_from_slice(&s.gates);
         let repr = c.repr();
         writeln!(file, "{}", repr)
             .expect("Failed to write to file");
