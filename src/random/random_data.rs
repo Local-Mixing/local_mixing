ea7957e83377aa59c0a90ce2208b69bacdf15894
honbugit <honicholas10@gmail.com>
2026-04-22 01:16:30 -0400

    try removing c1 > c2 requirement

--- src/random/random_data.rs
+++ src/random/random_data.rs
@@ -3374,7 +3374,7 @@ pub fn build_from_2rocks(
 
             let mut local_results: Vec<(CircuitSeq, Vec<Polynomial>, Vec<u8>, Vec<u8>)> = Vec::new();
 
-            for j in 0..=i {
+            for j in 0..=n {
                 let c2 = &db2_circuits_par[j];
                 let n2 = touched_wires(c2).len();
                 let c2_rev = CircuitSeq { gates: c2.gates.iter().rev().cloned().collect() };
