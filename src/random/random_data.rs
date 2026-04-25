b27984515af0e10e1bd214de19cd2c2b0fe54be6
honbugit <honicholas10@gmail.com>
2026-04-24 21:40:55 -0400

    Testing _4

--- src/random/random_data.rs
+++ src/random/random_data.rs
@@ -5871,10 +5871,8 @@ mod tests {
     #[test]
     fn test_c1_vs_c2_after_canon() {
         use crate::circuit::circuit::poly_to_str;
-        // let mut c1 = CircuitSeq { gates: vec![[3, 0, 2], [4, 1, 3]] };
-        // let mut c2 = CircuitSeq { gates: vec![[2, 1, 3], [4, 0, 2]] };
-        let mut c1 = CircuitSeq { gates: vec![[4, 1, 2], [1, 0, 3]]  };
-        let mut c2 = CircuitSeq { gates: vec![[4, 1, 3], [1, 0, 2]] };
+        let mut c1 = CircuitSeq { gates: vec![[2, 0, 1], [3, 1, 2]]  };
+        let mut c2 = CircuitSeq { gates: vec![[2, 1, 0], [3, 0, 2]] };
         let poly_1 = c1.to_polynomial(6, 0, 2);
         let poly_2 = c2.to_polynomial(6, 0, 2);
         println!("Original c1:");
