c05167d5b3006362eadb667063eadedaaa8b70a1
honbugit <honicholas10@gmail.com>
2026-04-25 01:15:05 -0400

    Testing _4

--- src/random/random_data.rs
+++ src/random/random_data.rs
@@ -6028,6 +6028,8 @@ println!("\nTotal circuits up to canonicalization AND reversal: {}", seen_canon.
         println!("{:?}", canonicalize_polys(c1.to_polynomial(9, 0, 3), true, false).0);
         let mut c1 = CircuitSeq { gates: vec![[5, 4, 2], [1, 4, 3], [4, 0, 1] ]  };
         let mut c2 = CircuitSeq { gates: vec![[3, 0, 2], [5, 0, 4], [0, 1, 3]]  };
+        c1.gates.reverse();
+        c2.gates.reverse();
         let poly_1 = c1.to_polynomial(9, 0, 3);
         let poly_2 = c2.to_polynomial(9, 0, 3);
         println!("Original c1:");
