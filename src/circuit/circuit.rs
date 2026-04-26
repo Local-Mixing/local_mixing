6c6d24f6ccd81ccde49e02cce35bb26875777f5d
honbugit <honicholas10@gmail.com>
2026-04-26 01:48:23 -0400

    Remove separate reversal check

--- src/circuit/circuit.rs
+++ src/circuit/circuit.rs
@@ -878,6 +878,14 @@ impl CircuitSeq {
             c1.rewire(&canon1.1.invert(), n);
             c1.canonicalize();
             (canon1.0, c1)
+        } else if poly_vec_key(&canon1.0) > poly_vec_key(&canon2.0){
+            c2.rewire(&canon2.1.invert(), n);
+            c2.canonicalize();
+            (canon2.0, c2)
+        } else if c1.gates <= c2.gates {
+            c1.rewire(&canon1.1.invert(), n);
+            c1.canonicalize();
+            (canon1.0, c1)
         } else {
             c2.rewire(&canon2.1.invert(), n);
             c2.canonicalize();
