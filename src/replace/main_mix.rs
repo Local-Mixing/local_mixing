c0f8d925bd9ec7a8015156eaebf62e08e2bbfb08
honbugit <honicholas10@gmail.com>
2026-05-20 04:18:19 -0400

    Fixing equality checks after gadgetizing

--- src/replace/main_mix.rs
+++ src/replace/main_mix.rs
@@ -1022,7 +1022,7 @@ pub fn main_shuffle_shoot_shuffle(
         loop {
             let mut shuffled = circuit.clone();
             insert_wire_m_samfs_every_x(&mut shuffled, n, n, 1, env, &dbs);
-            if shuffled.probably_equal(&circuit, orig_n, 100).is_ok() {
+            if shuffled.probably_equal(&circuit, n, 100).is_ok() {
                 circuit = shuffled;
                 break;
             }
@@ -1060,7 +1060,7 @@ pub fn main_shuffle_shoot_shuffle(
                     1,
                     curated,
                 );
-                if new_circuit.probably_equal(&circuit, orig_n, 100).is_ok() {
+                if new_circuit.probably_equal(&circuit, n, 100).is_ok() {
                     circuit = new_circuit;
                     break;
                 }
@@ -1070,7 +1070,7 @@ pub fn main_shuffle_shoot_shuffle(
         let mut new_circuit = circuit.clone();
         loop {
             insert_wire_m_samfs_every_x(&mut new_circuit, n, m, x, env, &dbs);
-            if new_circuit.probably_equal(&circuit, orig_n, 100).is_ok() {
+            if new_circuit.probably_equal(&circuit, n, 100).is_ok() {
                 circuit = new_circuit;
                 break;
             }
