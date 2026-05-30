84ade3fe5a6149a74a10dd9a85bce2b0c37f3876
Eli Baum <elibaum@bu.edu>
2026-05-30 14:11:08 +0000

    point function

--- src/bin/point_function.rs
+++ src/bin/point_function.rs
@@ -1,8 +1,9 @@
 // Build a point function (or an identity that looks like one!)
 
-use std::ops::Range;
+use std::{ops::Range, slice::IterMut};
 
-use clap::{Parser};
+use clap::Parser;
+use itertools::chain;
 use local_mixing::circuit::{CircuitSeq, Gate};
 
 #[derive(Parser, Debug)]
@@ -15,32 +16,66 @@ struct Args {
     key: usize,
 
     #[arg(short, long, default_value_t = false)]
-    identity: bool
+    identity: bool,
 }
 
-fn tof_to_g57(wires: u8, g: Gate) -> CircuitSeq {
+fn tof_to_g57(wires: u8, g: &[u8; 3]) -> Vec<[u8; 3]> {
     // Let r be a random wire from the range 0..wires, NOT including any of the values in g.
     let mut r = fastrand::u8(0..wires);
-    while g.pins.contains(&r) {
+    while g.contains(&r) {
         r = fastrand::u8(0..wires);
     }
 
-    let active = g.pins[0] as u8;
-    let ctrl1 = g.pins[1] as u8;
-    let ctrl2 = g.pins[2] as u8;
+    let active = g[0] as u8;
+    let ctrl1 = g[1] as u8;
+    let ctrl2 = g[2] as u8;
 
-    CircuitSeq { gates: vec![
+    vec![
         [active, ctrl1, r],
         [ctrl2, ctrl1, r],
         [active, ctrl1, ctrl2],
-        [ctrl2, ctrl2, r]
-    ]}
+        [ctrl2, ctrl1, r],
+    ]
 }
 
-fn big_tof(active: u8, controls: Range<u8>) {
+fn big_tof(n: u8, active: u8, controls: Range<u8>) -> CircuitSeq {
     // Build the staircase
-}
+    let mut empty: Vec<u8> = (0..n + 2).collect();
 
+    // Delete index `active`, and all indices in `controls` from the vector
+    empty.retain(|x| *x != active && !controls.contains(x));
+
+    // Shuffle the remaining indices
+    fastrand::shuffle(&mut empty);
+
+    let mut ctrl: Vec<u8> = controls.collect();
+
+    let stair: Vec<[u8; 3]> = (1..(ctrl.len() - 2))
+        .map(|i| [empty[i - 1], ctrl[i], empty[i]])
+        .collect();
+
+    let mut rev_stair = stair.clone();
+    rev_stair.reverse();
+
+    let last_tof = empty[ctrl.len() - 3];
+
+    let base_gate = [active, ctrl[0], empty[0]];
+    let top_gate = [last_tof, ctrl[ctrl.len() - 2], ctrl[ctrl.len() - 1]];
+
+    CircuitSeq {
+        gates: chain![
+            tof_to_g57(n, &base_gate),
+            stair.iter().flat_map(|g| { tof_to_g57(n, g) }),
+            tof_to_g57(n, &top_gate),
+            rev_stair.iter().flat_map(|g| { tof_to_g57(n, g) }),
+            tof_to_g57(n, &base_gate),
+            stair.iter().flat_map(|g| { tof_to_g57(n, g) }),
+            tof_to_g57(n, &top_gate),
+            rev_stair.iter().flat_map(|g| { tof_to_g57(n, g) }),
+        ]
+        .collect(),
+    }
+}
 
 fn main() {
     let args = Args::parse();
@@ -54,12 +89,20 @@ fn main() {
 
     let b = n.div_ceil(2);
 
+    // TODO: Insert the NOTs
+
+    let pf = big_tof(n, n, 0..b)
+        .concat(&big_tof(n, n + 1, b..n + 1))
+        .concat(&big_tof(n, n, 0..b))
+        .concat(&big_tof(n, n + 1, b..n + 1));
+
+    // TODO: id or pf
     // Insert the NOTs
 
-    big_tof(n, 0..b);
-    big_tof(n+1, b..n+1);
-    big_tof(n, 0..b);
-    big_tof(n+1, b..n+1);
+    println!("{}", pf.repr());
 
-    // Insert the NOTs
+    println!("0 => {}", pf.evaluate(0));
+    let r = fastrand::usize(0..(1 << n));
+    println!("{} => {}", r, pf.evaluate(r));
+
 }
