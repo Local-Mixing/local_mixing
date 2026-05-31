0991f690e46c3021f4ee507888fd0cbf6236f4d4
honbugit <honicholas10@gmail.com>
2026-05-31 15:29:01 -0400

    Adding incremental heatmaps

--- src/lib.rs
+++ src/lib.rs
@@ -133,6 +133,87 @@ fn heatmap(
 }
 
 #[pyfunction]
+fn heatmap_incremental(
+    py: Python<'_>,
+    num_wires: usize,
+    num_inputs: usize,
+    flag: bool,
+    c1: &str,
+    c2: &str,
+    canon: bool,
+    _fix: usize,
+    hw: bool,
+) -> Py<PyArray2<f64>> {
+    let mask = if num_wires < 256 {
+        (u256::one() << num_wires) - u256::one()
+    } else {
+        u256::MAX
+    };
+    println!("Running incremental heatmap on {} inputs (random base + increments)", num_inputs);
+    io::stdout().flush().unwrap();
+    let circuit_one_str = fs::read_to_string(c1).expect("Failed to read c1");
+    let circuit_two_str = fs::read_to_string(c2).expect("Failed to read c2");
+    let mut circuit_one = CircuitSeq::from_string(&circuit_one_str);
+    let mut circuit_two = CircuitSeq::from_string(&circuit_two_str);
+    if canon {
+        circuit_one.canonicalize();
+        circuit_two.canonicalize();
+    }
+    let circuit_one_len = circuit_one.gates.len();
+    let circuit_two_len = circuit_two.gates.len();
+
+    let num_points = (circuit_one_len + 1) * (circuit_two_len + 1);
+    let mut average = vec![0f64; num_points * 3];
+    let mut rng = rand::rng();
+    let start_time = Instant::now();
+    // One random base input; subsequent inputs are x0+1, x0+2, ... (mod 2^num_wires).
+    let x0: u256 = (u256::from(rng.random::<u128>())
+                | (u256::from(rng.random::<u128>()) << 128)) & mask;
+    for i in 0..num_inputs {
+        if i % 10 == 0 {
+            println!("{}/{}", i, num_inputs);
+            io::stdout().flush().unwrap();
+        }
+        let input_bits = x0.overflowing_add(u256::from(i as u128)).0 & mask;
+
+        let evolution_one = circuit_one.evaluate_evolution_256(input_bits);
+        let evolution_two = circuit_two.evaluate_evolution_256(input_bits);
+
+        for i1 in 0..=circuit_one_len {
+            for i2 in 0..=circuit_two_len {
+                let hamming_dist = if hw {
+                    (popcount_u256(evolution_one[i1]) as f64 - popcount_u256(evolution_two[i2]) as f64).abs()
+                } else {
+                    let diff = (evolution_one[i1] ^ evolution_two[i2]) & mask;
+                    popcount_u256(diff) as f64
+                };
+                let overlap = if !flag || hw {
+                    hamming_dist / num_wires as f64
+                } else {
+                    let tmp = (2.0 * hamming_dist / num_wires as f64) - 1.0;
+                    tmp.abs()
+                };
+                let index = i1 * (circuit_two_len + 1) + i2;
+                average[index * 3] = i1 as f64;
+                average[index * 3 + 1] = i2 as f64;
+                average[index * 3 + 2] += overlap / num_inputs as f64;
+            }
+        }
+    }
+
+    println!("Time elapsed: {:?}", Instant::now() - start_time);
+
+    let mut arr2 = Array2::<f64>::zeros((num_points, 3));
+    for i in 0..num_points {
+        arr2[[i, 0]] = average[i * 3];
+        arr2[[i, 1]] = average[i * 3 + 1];
+        arr2[[i, 2]] = average[i * 3 + 2];
+    }
+    let pyarray = PyArray2::from_owned_array(py, arr2);
+    pyarray.into()
+}
+
+#[pyfunction]
 fn heatmap_small(
     py: Python<'_>,
     num_wires: usize, 
@@ -413,6 +494,7 @@ fn heatmap_mini_slice(
 fn local_mixing(module: &Bound<'_, PyModule>) -> PyResult<()> {
     // wrap the function, passing the module `m`
     module.add_function(wrap_pyfunction!(heatmap, module)?)?;
+    module.add_function(wrap_pyfunction!(heatmap_incremental, module)?)?;
     module.add_function(wrap_pyfunction!(heatmap_small, module)?)?;
     module.add_function(wrap_pyfunction!(heatmap_slice, module)?)?;
     module.add_function(wrap_pyfunction!(heatmap_mini_slice, module)?)?;
